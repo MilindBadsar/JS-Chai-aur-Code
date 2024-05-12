6e7b1187e03075d738c777b8fb2f256fe9098c18
MilindBadsar <milindbadsar@gmail.com>
2024-05-12 23:50:01 +0000

    basics done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,23 @@
 console.log("Milind")
+
+console.log("Milind")
+
+const accountId = 12345
+let accountEmail = "milind@gmail.com"
+var accountPassword = "454545"
+accountCity = "Jaipur"
+let accountState;
+
+// can't change const value
+/*
+prefer not use var
+because of issue with block scope and functional scope
+*/
+
+accountEmail = "badsar@gmail.com"
+accountPassword = "212121"
+accountCity = "Noida"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
