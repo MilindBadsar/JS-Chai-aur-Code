b927af5cc089e0ee5ff9323c7f9d7d388bd4a704
MilindBadsar <milindbadsar@gmail.com>
2024-05-14 08:27:02 +0000

    updated

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Milind"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof age)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "Milind"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// conversion to Boolean
+// 1 => true; 0 => false
+// "" => false
+// "milind" => true
